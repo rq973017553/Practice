8f47c310d16bdc12e2614413a62c96dd64b5d2cd
饶强 <qiangrao@pptv.com>
2020-06-08 02:32:49 +0800

    gilde练习

--- PracticeDemo/src/main/java/com/rq/practice/activities/practice/GlidePracticeActivity.java
+++ PracticeDemo/src/main/java/com/rq/practice/activities/practice/GlidePracticeActivity.java
@@ -1,18 +1,9 @@
 package com.rq.practice.activities.practice;
 
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import android.media.MediaMetadataRetriever;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.GlideBuilder;
-import com.bumptech.glide.request.target.CustomTarget;
-import com.bumptech.glide.request.target.CustomViewTarget;
-import com.bumptech.glide.request.target.ViewTarget;
-import com.bumptech.glide.request.transition.Transition;
 import com.rq.practice.R;
 import com.rq.practice.activities.base.BaseToolBarActivity;
 
@@ -34,9 +25,11 @@ public class GlidePracticeActivity extends BaseToolBarActivity {
 
     @Override
     public void initData() {
-        Glide.with(GlidePracticeActivity.this)
-             .load(URL)
-             .placeholder(R.drawable.xiaomai)
-             .into(testGlide);
+        Glide.with(this)
+//                .asGif()
+//                .asBitmap()
+                .load("http://attach.bbs.miui.com/forum/201408/07/194456i55q58pqnb55fi88.jpg")
+                .thumbnail(0.1f)
+                .into(testGlide);
     }
 }
---
+++ PracticeDemo/src/main/java/com/rq/practice/config/GlideConfig.java
@@ -0,0 +1,37 @@
+package com.rq.practice.config;
+
+import android.content.Context;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.GlideBuilder;
+import com.bumptech.glide.Registry;
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.load.engine.cache.DiskLruCacheFactory;
+import com.bumptech.glide.module.AppGlideModule;
+
+import java.io.File;
+
+@GlideModule
+public class GlideConfig extends AppGlideModule {
+
+    @Override
+    public boolean isManifestParsingEnabled() {
+        return false;
+    }
+
+    @Override
+    public void applyOptions(@NonNull Context context, @NonNull GlideBuilder builder) {
+        if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())){
+            File externalFilesDir = Environment.getExternalStorageDirectory();
+            if (externalFilesDir != null){
+                builder.setDiskCache(new DiskLruCacheFactory(externalFilesDir.getAbsolutePath(),"imageCache", 100*1024*1024));
+            }
+        }
+    }
+
+    @Override
+    public void registerComponents(@NonNull Context context, @NonNull Glide glide, @NonNull Registry registry) {
+    }
+}
