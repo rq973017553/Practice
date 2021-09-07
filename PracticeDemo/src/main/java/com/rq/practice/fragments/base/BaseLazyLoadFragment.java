cfd47d04cfaab70266e173163a79685d99eb715c
饶强 <qiangrao@pptv.com>
2021-09-08 02:37:44 +0800

    1.NativeRecyclerActivity添加打印 2.修改OKHttp3的版本 3.修改BaseLazyLoadFragment,解决懒加载不正确的问题

--- PracticeDemo/src/main/java/com/rq/practice/fragments/base/BaseLazyLoadFragment.java
+++ PracticeDemo/src/main/java/com/rq/practice/fragments/base/BaseLazyLoadFragment.java
@@ -3,9 +3,7 @@ package com.rq.practice.fragments.base;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 
 /**
  * 懒加载BaseFragment
@@ -13,38 +11,40 @@ import android.view.ViewGroup;
  * 导致无法配合onResume和onStop统计用户查看
  * (2)有可能预加载的页面需要加载大量图片，加载比较耗时。
  * (3)如果出现页面空白的情况，可以复现ViewPager的destroyItem，同时去掉super方法
- * 参考链接:https://www.jianshu.com/p/254dc5ddffea
+ * 参考链接:https://www.jianshu.com/p/0e2d746e3a3d
+ * 参考链接:https://www.jianshu.com/p/2201a107d5b5
  * @author rock you
  * @version [1.0.0 2018.8.16]
  */
 public abstract class BaseLazyLoadFragment extends BaseFragment{
 
-    private boolean isCreateView = false;
+    private volatile boolean isCreateView = false;
 
-    private boolean isShowUI = false;
+    private volatile boolean isShowUI = false;
 
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
-    }
-
-    // onCreateView -> onViewCreated -> onActivityCreated
+    // onCreateView-->onViewCreated-->onActivityCreated
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        if (mView != null) {
-            isCreateView = true;
+        isCreateView = true;
+        doLazyLoad();
+    }
+
+    public void doLazyLoad(){
+        if (isShowUI && isCreateView){
+            lazyLoad();
         }
     }
 
+    /**
+     * 该方法在add+hide+show的情形未测试懒加载
+     * @param hidden 是否隐藏
+     */
     @Override
-    public void initData() {
-        if (isShowUI && isCreateView){
-            lazyLoad();
-            isCreateView = false;
-            isShowUI = false;
-        }
+    public void onHiddenChanged(boolean hidden) {
+        super.onHiddenChanged(hidden);
+        this.isShowUI = !hidden;
+        doLazyLoad();
     }
 
     /**
@@ -54,18 +54,16 @@ public abstract class BaseLazyLoadFragment extends BaseFragment{
      */
     @Override
     public void setUserVisibleHint(boolean isVisibleToUser) {
-        if (mView == null) {
-            return;
-        }
-        if (isVisibleToUser){
-            // fragment可见
-            isShowUI = true;
-            initData();
-        }else {
-            // fragment不可见
-            isShowUI = false;
-        }
+        this.isShowUI = isVisibleToUser;
+        doLazyLoad();
     }
 
     public abstract void lazyLoad();
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        isShowUI = false;
+        isCreateView = false;
+    }
 }
